543b2489074530f0542aa37e67c1d78c2e650a9e
mhebing <marcel.hebing@gmail.com>
2023-01-11 17:16:44 +0100

    Update tasks

--- tasks.py
+++ tasks.py
@@ -3,3 +3,28 @@ from invoke import task
 @task
 def hi(c):
     print("Hi")
+
+@task
+def setup(c):
+    print("[INFO] Setup")
+    c.run("mkdir input")
+    c.run("mkdir output")
+
+@task
+def clear(c):
+    print("[INFO] Clear")
+    c.run("rm -rf input")
+    c.run("rm -rf output")
+    c.run("rm -rf _build")
+
+@task
+def html(c):
+    print("[INFO] Make HTML")
+    c.run("make html")
+
+@task
+def full(c):
+    print("[INFO] Full run")
+    clear(c)
+    setup(c)
+    html(c)
