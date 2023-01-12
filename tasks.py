c52f718c44fafac3c26dd34e4701fb0f35e77c60
mhebing <marcel.hebing@gmail.com>
2023-01-12 15:31:19 +0100

    Comment tasks

--- tasks.py
+++ tasks.py
@@ -3,15 +3,18 @@ import conf
 
 @task
 def hi(c):
+    """Say hi"""
     print("Welcome to the ADS demo project.")
 
 @task
 def download_data(c):
+    """Download the dataset"""
     print("[INFO] Download data")
     c.run(f"cd input && wget {conf.data_url}")
 
 @task
 def setup(c):
+    """(!) Basic setup"""
     print("[INFO] Setup")
     c.run("mkdir input")
     c.run("mkdir output")
@@ -20,6 +23,7 @@ def setup(c):
 
 @task
 def clear(c):
+    """Clear local directories and files"""
     print("[INFO] Clear")
     c.run("rm -rf input")
     c.run("rm -rf output")
@@ -28,16 +32,19 @@ def clear(c):
 
 @task
 def apidoc(c):
+    """Generate documentation of lib_py"""
     print("[INFO] Create apidoc")
     c.run("sphinx-apidoc lib_py -o temp/apidoc")
 
 @task
 def html(c):
+    """Run Sphinx and generate HTML"""
     print("[INFO] Make HTML")
     c.run("make html")
 
 @task
 def full(c):
+    """(!) Clear project and run everything"""
     print("[INFO] Full run")
     clear(c)
     setup(c)
